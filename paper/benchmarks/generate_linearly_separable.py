f7498c9030ad820ea5268c5dc08084dcbd5690cf
TheLordNight <117924447+TheLordNight@users.noreply.github.com>
2025-06-12 01:24:45 +0200

    amended licensing header

--- paper/benchmarks/generate_linearly_separable.py
+++ paper/benchmarks/generate_linearly_separable.py
@@ -1,4 +1,5 @@
 # Copyright 2024 Xanadu Quantum Technologies Inc.
+# Copyright 2025 Chaitanya Agrawal
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This file has been modified by Chaitanya Agrawal from the original version.
 
 """Generate datasets for the  LINEARLY SEPARABLE benchmark."""
 
